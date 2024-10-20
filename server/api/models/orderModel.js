445822ab0e0365d2eac24ab4cb463d9c020463be
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-20 19:02:14 +0530

    order model is created

---
+++ server/api/models/orderModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    product : [{
+        product : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Product',
+            required : true
+        },
+        quantity : {
+            type: Number,
+            required : true,
+            default : 1,
+        }
+    }],
+    transactionCode : {
+        type: String,
+        required: true,
+        unique : true
+    },
+    totalAmount : {
+        type: Number,
+        required: true
+    },
+    shippingAddress : {
+        addressLine1: String,
+        addressLine2: String,
+        city:String,
+        state: String,
+        postalCode:String,
+        country:String
+    },
+    status : {
+        type: String,
+        enum : ['pending','completed','shipped','delivered','cancelled'],
+        default : 'pending'
+    },
+
+    createdAt : {
+        type: Date,
+        default : Date.now
+    }
+},{
+    timestamps : true
+});
+
+const Order = mongoose.model('Order',OrderSchema);
+module.exports = Order;
