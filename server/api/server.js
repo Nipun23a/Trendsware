647f9a27536e6d634c0a92679c8873f4f5f3bb46
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-21 01:22:57 +0530

    Server js file updated

--- server/api/server.js
+++ server/api/server.js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require("express");
 const cors = require('cors');
 const dotenv = require('dotenv')
 const connectDB = require('./config/db');
-
 // Import Routes
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 dotenv.config();
-
 const app = express();
 
 // Middleware
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 connectDB();
 
-app.use('/api/users',userRoutes);
-app.use('/api/products',productRoutes);
-app.use('/api/orders',orderRoutes);
+app.get('/', (req, res) => {
+    res.send('Welcome to the Trendsware API');
+});
+ 
+app.get('/test', (req, res) => {
+    res.json({ message: 'Server is working correctly!' });
+});
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
