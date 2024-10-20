c0106fe757eba843e1264808aa48badc974de03b
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-20 15:39:21 +0530

    db.js , server.js  and firebase-admin.js is updated

--- server/api/config/firebase-admin.js
+++ server/api/config/firebase-admin.js
@@ -6,4 +6,8 @@ const serviceAccount = require(path.json(__dirname,'serviceAccount.json'));
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://trendsware-b1b13-default-rtdb.firebaseio.com"
-})
+});
+
+module.exports = admin;
+
+
