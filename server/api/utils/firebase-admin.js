9511a0f6d1755059e7beaac2ef34cef743f301f6
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-20 14:02:57 +0530

    Firebase intergratoed to frontend and backend

---
+++ server/api/utils/firebase-admin.js
@@ -0,0 +1,9 @@
+const admin = require('firebase-admin');
+const path = require('path');
+
+const serviceAccount = require(path.json(__dirname,'serviceAccount.json'));
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://trendsware-b1b13-default-rtdb.firebaseio.com"
+})
