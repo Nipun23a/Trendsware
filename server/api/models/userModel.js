0b54fed805fc290aafedaf3cb31277eec867946d
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-20 21:28:35 +0530

    Added User Role for

--- server/api/models/userModel.js
+++ server/api/models/userModel.js
@@ -5,8 +5,8 @@ const UserSchema = new mongoose.Schema({
     first_name: {type:String,required:true},
     last_name : {type: String,required:true},
     username : {type: String,required: true},
-    password : {type: String, required : true}
-
+    password : {type: String, required : true},
+    user_role : {type: String, enum : ['admin','customer']}
 }, { timestamps:true });
 
 
