5b5351e0dfc284a46159ad86e8299b425201b0a9
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-22 23:53:33 +0530

    tailwind config updated registerd the fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lexend': ['"Lexend Giga"', 'sans-serif'],
+        'montserrat': ['"Montserrat"', 'sans-serif'],
+        'raleway': ['"Raleway"', 'sans-serif'],
+        'poppins': ['"Poppins"', 'sans-serif'],
+        'libre': ['"Libre Baskerville"', 'serif'],
+        'rouge': ['"Rouge Script"', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
