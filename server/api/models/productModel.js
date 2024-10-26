756ce4c2e95553491a1b8b835c7c5e75d7db0c9f
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-26 20:21:42 +0530

    Product Model Gender Enum Removed And Quantity Updated

--- server/api/models/productModel.js
+++ server/api/models/productModel.js
@@ -4,10 +4,9 @@ const productSchema = new mongoose.Schema({
     name:{type:String,required: true},
     description : {type:String,required:true},
     price : {type: Number, required: true},
+    quantity : {type: Number , required: true},
     imageURL : {type:String, required: true},
     category : {type: String, required: true},
-    gender : {type: String, enum: ['male','female','unisex'], required: true},
-
 }, {timestamps:true});
 
 module.exports = mongoose.model('Product', productSchema);
