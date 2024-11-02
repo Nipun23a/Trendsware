70525608bee2f904974b16899d0efceabcd31bdc
Nipuna Kaweya <nipuna609@gmail.com>
2024-11-02 15:13:05 +0530

    Customer Testimonial About Container Added

--- client/src/pages/common-pages/About.jsx
+++ client/src/pages/common-pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutUpperContainer from "../../components/about/about-upper-container";
 import OurStoryContainer from "../../components/about/our-story-container";
 import AcheivmentContainer from "../../components/about/acheivment-container";
+import CustomerTestomonialContainer from "../../components/about/customer-testomonial-container";
 
 
 
@@ -21,6 +22,11 @@ const About = () => {
             <section className="bg-[#BAD7F2]">
                 <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
                     <AcheivmentContainer/>
+                </div>
+            </section>
+            <section className="bg-white py-16">
+                <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+                    <CustomerTestomonialContainer/>
                 </div>
             </section>
         </div>
