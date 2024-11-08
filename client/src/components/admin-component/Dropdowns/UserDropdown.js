781a1aaa93c7648ab8c1fe035da25688b03d245c
Nipuna Kaweya <nipuna609@gmail.com>
2024-11-08 14:47:49 +0530

    USer Dropdown CSS Updated

--- client/src/components/admin-component/Dropdowns/UserDropdown.js
+++ client/src/components/admin-component/Dropdowns/UserDropdown.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createPopper } from "@popperjs/core";
+import team from "../../../assets/img/team-1-800x800.jpg";
 
 const UserDropdown = () => {
   // dropdown props
@@ -27,11 +28,11 @@ const UserDropdown = () => {
         }}
       >
         <div className="items-center flex">
-          <span className="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full">
+          <span className="w-12 h-12 text-sm text-white bg-blue-200 inline-flex items-center justify-center rounded-full">
             <img
               alt="..."
               className="w-full rounded-full align-middle border-none shadow-lg"
-              src={require("assets/img/team-1-800x800.jpg").default}
+              src={team}
             />
           </span>
         </div>
